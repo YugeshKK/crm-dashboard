3c6778e25fc9ffd574fe3acc60313dbe85b117fc
Yugesh K K <yugeshkk2001@gmail.com>
2026-09-09 17:16:05 +0530

    Product details page ui

---
+++ src/pages/productDetails/productsDetailsTabs/ProductOverview.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface  ProductOverviewProps {
+}
+
+const ProductOverview: React.FunctionComponent< ProductOverviewProps> = (props) => {
+  return (
+    <div>
+      Hello overview 
+    </div>
+  );
+};
+
+export default ProductOverview;
